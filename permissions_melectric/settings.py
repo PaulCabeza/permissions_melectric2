7c6e5cb2bd655807df43ec0406231f5beee3f416
Paul Cabeza <ovidio.cabeza@gmail.com>
2022-11-24 05:55:36 -0600

    App ready to push it to render

--- permissions_melectric/settings.py
+++ permissions_melectric/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w0e0$yjmk5ajq*b)jsfy@()49)79)q&-yqpa*5o^l72um0ij*8'
+# SECRET_KEY = 'django-insecure-w0e0$yjmk5ajq*b)jsfy@()49)79)q&-yqpa*5o^l72um0ij*8'
+SECRET_KEY = os.environ.get('SECRET KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:    
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -42,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -81,16 +90,23 @@ WSGI_APPLICATION = 'permissions_melectric.wsgi.application'
 #     }
 # }
 
-DATABASES = {     
-'default': {
- 'ENGINE': 'django.db.backends.postgresql_psycopg2',
- 'NAME': 'permissions_melectric2_dev',
- 'USER': 'postgres',
- 'PASSWORD': 'postgres',
- 'HOST': 'localhost',
- 'PORT': ''  
-} 
-} 
+# DATABASES = {     
+# 'default': {
+#  'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#  'NAME': 'permissions_melectric2_dev',
+#  'USER': 'postgres',
+#  'PASSWORD': 'postgres',
+#  'HOST': 'localhost',
+#  'PORT': ''  
+# } 
+# } 
+
+DATABASES = { 
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/permissions_melectric2_dev',
+        conn_max_age=600
+        )
+        }
 
 
 # Password validation
